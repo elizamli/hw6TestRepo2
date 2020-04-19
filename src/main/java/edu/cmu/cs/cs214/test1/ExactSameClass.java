97e1f6f0aeb548c2e664717cbad696b1d438459c
Eliza Li <elizal@andrew.cmu.edu>
2020-04-19 19:56:56 -0400

    added attribute

--- src/main/java/edu/cmu/cs/cs214/test1/ExactSameClass.java
+++ src/main/java/edu/cmu/cs/cs214/test1/ExactSameClass.java
@@ -4,6 +4,7 @@ public class ExactSameClass {
     private int meepleCount;
     private final int playerId;
     private int score;
+    private static final String diffWord= "hello whaddup";
 
     private static final int INIT_MEEPLE_COUNT = 7;
 
