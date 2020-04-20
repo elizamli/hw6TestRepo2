396798b682e5b40298f9d54af89aabc426a3cfd5
Eliza Li <elizal@andrew.cmu.edu>
2020-04-19 22:02:42 -0400

    added code to classin Package

--- src/main/java/edu/cmu/cs/cs214/test1/pkg/ClassInPackage.java
+++ src/main/java/edu/cmu/cs/cs214/test1/pkg/ClassInPackage.java
@@ -1,5 +1,10 @@
 package edu.cmu.cs.cs214.test1.pkg;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
 public class ClassInPackage {
     int first = 0;
 
@@ -7,4 +12,26 @@ public class ClassInPackage {
     public int getFirst(){
         return first;
     }
+
+    enum SegmentType {
+        MONASTERY,
+        CITY,
+        ROAD,
+        FIELD;
+
+        /** copied from textbook Effective Java Item 34 */
+        private static final Map<String, SegmentType> STRING_TO_ENUM =
+                Stream.of(values()).collect(toMap(Object::toString, e -> e));
+
+        /**
+         * Returns the SegmentType enum value of a string.
+         * @param symbol the string
+         * @return enum of the string
+         */
+        public static SegmentType fromString(String symbol){
+            return STRING_TO_ENUM.get(symbol);
+        }
+
+    }
+
 }
